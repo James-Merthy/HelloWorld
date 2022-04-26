d49cb0cb3b42a0d26a34b5bfdd78eaa156236109
james <anasala.merthy@gmail.com>
2022-04-26 18:57:58 +0200

    Initial commit

---
+++ src/Hello_World.java
@@ -0,0 +1,14 @@
+public class Hello_World {
+
+    public static void main(String[] args) {
+
+        System.out.println("Salut");
+        System.out.println("Hello World");
+        System.out.println();
+        int i = 10000;
+
+        System.out.println(i);
+
+
+    }
+}
